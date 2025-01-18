c0f64e86acb14b3bff32f3d26c10be811d2e10d7
DB6KT <db6kt@darc.de>
2025-01-18 14:32:38 +0000

    Update RepeaterLogic.h

--- src/svxlink/svxlink/RepeaterLogic.h
+++ src/svxlink/svxlink/RepeaterLogic.h
@@ -121,16 +121,9 @@ class RepeaterLogic : public Logic
 {
   public:
     /**
-     * @brief 	Constuctor
-     * @param 	cfg A previously initialized config object
-     * @param 	name The name of this logic
+     * @brief 	Default constructor
      */
-    RepeaterLogic(Async::Config& cfg, const std::string& name);
-
-    /**
-     * @brief 	Destructor
-     */
-    ~RepeaterLogic(void);
+    RepeaterLogic(void);
 
     /**
      * @brief 	Initialize this logic
@@ -212,9 +205,8 @@ class RepeaterLogic : public Logic
     std::string     open_reason;
     int		    ident_nag_min_time;
     Async::Timer    ident_nag_timer;
-    Async::Timer    sql_flap_block_timer;
-    int             sql_flap_block_time;
-    bool            repeater_up_blocked;
+    uint32_t        delayed_tg_activation;
+    Async::Timer    open_on_ctcss_timer;
 
     void idleTimeout(Async::Timer *t);
     void setIdle(bool idle);
@@ -225,7 +217,6 @@ class RepeaterLogic : public Logic
     void openOnSqlTimerExpired(Async::Timer *t);
     void activateOnOpenOrClose(SqlFlank flank);
     void identNag(Async::Timer *t);
-    void blocktimeExpired(Async::Timer *t);
 
 };  /* class RepeaterLogic */
 
